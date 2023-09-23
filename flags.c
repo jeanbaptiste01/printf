68c4dbff59a8772a962a9aa397a45096797dec80
jeanbaptiste01 <muhayejean7@gmail.com>
2023-09-23 16:25:52 -0700

    flags

--- flags.c
+++ flags.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
+ * get_flags - Calculate active flags
  * by Jean Baptiste Muhayimana and Shehab Magdy
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
