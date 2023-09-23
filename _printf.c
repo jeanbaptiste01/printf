cf648d18225b6362372b165ed4cf814a157cbc69
jeanbaptiste01 <muhayejean7@gmail.com>
2023-09-23 16:20:23 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -6,7 +6,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * _printf - Printf function
  * @format: format.
  * by Jean Baptiste Muhayimana and Shehab Magdy
- * Return: Printed char.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
