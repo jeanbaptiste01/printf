a32701dfd8ab61ba6956771d7789d130211f9fce
jeanbaptiste01 <muhayejean7@gmail.com>
2023-09-23 17:03:59 -0700

    _functions

--- print_functions.c
+++ print_functions.c
@@ -7,7 +7,7 @@
  * by Jean Baptiste Muhayimana and Shehab Magdy
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  Calculate active flags
  * @width: Width
  * @precision: Precision specification
  * @size: Size specifier
